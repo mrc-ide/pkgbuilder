953f51ae30e2620b0c848ee3c97b828cb15f73a7
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-08-26 14:55:39 +0100

    Add integration test for worker

---
+++ tests/testthat/test-z-queue.R
@@ -0,0 +1,25 @@
+context("integration: queue")
+
+test_that("run a job in a queue", {
+  workdir <- tempfile()
+  version <- r_version2()
+
+  q <- queue$new(version, workdir)
+  id <- q$submit(version, "reside-ic/defer", NULL)
+
+  w <- pb_worker(workdir)
+  w()
+
+  expect_equal(
+    q$status(version, id),
+    list(status = "COMPLETE", log = NULL))
+
+  res <- q$status(version, id, TRUE)
+  expect_equal(res$status, "COMPLETE")
+  expect_is(res$log, "character")
+  expect_true(length(res$log) > 10)
+
+  res <- q$result(version, id)
+  expect_true(file.exists(res))
+  expect_match(basename(res), "defer_[0-9]+\\.[0-9]+\\.[0-9]+\\.(tgz|zip)$")
+})
